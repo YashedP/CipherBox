2f2144f077e497304d1b768db994f0216abbe90e
Yash Jani <yashjani144@gmail.com>
2025-10-03 03:09:12 -0400

    design plan for myself for the left panel

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -7,6 +7,12 @@ interface LeftPanelProps {
   onOptionsChange: (options: TransformOptions<TransformationType>) => void
 }
 
+// The entire list will be wrapped in a shadcn scroll area
+// Design, use shadcn tooltip on hover for each transformation type that shows the description of the transformation (Use TooltipProvider, Tooltip, TooltipTrigger, TooltipContent, and source text from transformationDescriptions)
+// Have a settings icon on the right side of each transformation type with a settings svg icon which when clicked, opens a shadcn dialog
+// When clicked saved in the dialog, the options will be saved and then the transformation will be applied, and an shadcn sonner (toast) will be shown to the user indicating that it has been saved
+// Have a decently equally sized button which takes the size of the biggest text in the list of transformation types in a flexbox both horizontally and vertically (Use a vertical list (flex/grid). Each row: button with w-full, fixed vertical size (h-10 or h-12), inline-flex items-center justify-between, so text on left and settings icon on right. This avoids layout shifts if labels differ and is more standard. If truncation is needed, add truncate.)
+
 function LeftPanel({ }: LeftPanelProps) {
   return (
     <div className="w-full md:w-1/2 h-1/2 md:h-full bg-gray-50 flex items-center justify-center">
@@ -19,4 +25,3 @@ function LeftPanel({ }: LeftPanelProps) {
 }
 
 export default LeftPanel
-
